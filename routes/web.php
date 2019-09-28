ae31e44bd7d70f2ce205527a37f173f258b5f7b1
gamg <adolfz10@gmail.com>
2019-09-28 06:57:38 -0400

    Form and validation examples added

--- routes/web.php
+++ routes/web.php
@@ -45,5 +45,6 @@ Route::put('/form', 'ClientController@testingForm')->name('testform');
 
 Route::view('/guardar-datos', 'save_data');
 Route::post('/guardar-datos', 'ClientController@saveFromForm')->name('save');
+
 Route::view('/almacenar', 'store');
 Route::post('/almacenar', 'ClientController@clientStore')->name('client.store');
